c996b357e32659f41f7fe46a9f007a1795628656
Shubham Aggarwal <ShubhamAggarwal-external@WorldFamilyEnglish.com>
2025-01-14 19:22:52 +0530

    Day1: Learned Arrow functions, variables some other JS basics.

---
+++ day-1.js
@@ -0,0 +1,62 @@
+// Day-1 of Learning.JS
+
+console.log('This is my first javascript program')
+
+//string
+console.log('this is a string')
+
+//string concatenation
+console.log('bat'+'man')
+
+//addition of numbers in different ways
+console.log('1'+'1')
+console.log(1+1)
+
+// performing arithmetic operations ( Addition,subtraction,multiply,divide,modulus(remainder))
+console.log(10+5)
+console.log(10-5)
+console.log(10*5)
+console.log(10/5)
+console.log(10%5)
+
+//Boolean
+console.log(true)
+
+//variables
+const age=20
+console.log(age)
+let sum=0
+let num1=300
+let num2=50
+sum=num1+num2
+console.log(sum)
+
+//Function
+function hello(){
+    console.log('hello!')
+}
+hello()
+
+//passing values 
+function name(firstname,lastname){
+    console.log('first name is: '+firstname)
+    console.log('lastname is: '+lastname)
+}
+name('Dhruv','Gupta')
+name()
+function logger(arg){
+    console.log(arg)
+}
+logger('hello')
+
+//Arrow function with _
+const num = _ =>10
+const rslt = ten()
+console.log(rslt) 
+
+//Arrow function with ()
+const ten=()=>10
+const result =ten()
+console.log(result)
+
+
